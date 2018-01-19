801abc544bdce61f1c8d68edecbfad0e0f08f89c
I <patricksun2011@gmail.com>
2018-01-19 15:41:11 -0800

    Remove DOCKER_REPOSITORY_HOST from required env vars to build the UI

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,11 +1,9 @@
 const FLOTILLA_API = process.env.FLOTILLA_API || "FILL_ME_IN"
 const CLUSTERS_API = process.env.CLUSTERS_API || "config/clusters.json"
-const DOCKER_REPOSITORY_HOST = process.env.DOCKER_REPOSITORY_HOST || ""
 const DEFAULT_CLUSTER = process.env.DEFAULT_CLUSTER || "default"
 
 export default {
   FLOTILLA_API,
   CLUSTERS_API,
-  DOCKER_REPOSITORY_HOST,
   DEFAULT_CLUSTER,
 }
