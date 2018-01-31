efb1d97a6b5cc5f76fbcbf534eab913c28a4fb1f
I <patricksun2011@gmail.com>
2018-01-31 11:32:26 -0800

    Add skeleton for EnvFieldArray test

---
+++ ui/src/components/__tests__/EnvFieldArray.spec.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { reducer as formReducer } from "redux-form"
+import { createStore, combineReducers } from "redux"
+import { configureSetup } from "../../__testutils__"
+import EnvFieldArray from "../EnvFieldArray"
+
+const setup = configureSetup({
+  connected: EnvFieldArray,
+  unconnected: EnvFieldArray,
+})
+
+// @TODO: finish this.
+describe("EnvFieldArray", () => {
+  it("works", () => {
+    const wrapper = setup({
+      connectToRouter: true,
+      connectToReduxForm: true,
+      formName: "test",
+      store: createStore(combineReducers({ form: formReducer })),
+    })
+  })
+})
