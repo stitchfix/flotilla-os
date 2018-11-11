cf24e000aef726ec9a7da33eaf757e26159333b0
Patrick Sun <patricksun2011@gmail.com>
2018-11-11 13:15:40 -0800

    AsyncDataTable styles

--- ui/src/components/styled/AsyncDataTable.js
+++ ui/src/components/styled/AsyncDataTable.js
@@ -1,3 +1,4 @@
+import React from "react"
 import styled from "styled-components"
 import {
   SPACING_PX,
@@ -5,12 +6,10 @@ import {
   VIEW_HEADER_HEIGHT_PX,
 } from "../../constants/styles"
 
-const ASYNC_DATA_TABLE_FILTERS_WIDTH_PX = 300
+const ASYNC_DATA_TABLE_FILTERS_WIDTH_PX = 360
 
 export const AsyncDataTableContainer = styled.div`
   width: 100%;
-  overflow-y: scroll;
-  height: calc(100vh - ${TOPBAR_HEIGHT_PX}px - ${VIEW_HEADER_HEIGHT_PX}px);
 `
 
 export const AsyncDataTableFilters = styled.div`
@@ -18,10 +17,13 @@ export const AsyncDataTableFilters = styled.div`
   min-width: ${ASYNC_DATA_TABLE_FILTERS_WIDTH_PX}px;
   position: fixed;
   top: calc(${TOPBAR_HEIGHT_PX}px + ${VIEW_HEADER_HEIGHT_PX}px);
-  right: 0;
+  left: 0;
+  bottom: 0;
 `
 
 export const AsyncDataTableContent = styled.div`
   flex: 1;
-  margin-right: ${ASYNC_DATA_TABLE_FILTERS_WIDTH_PX}px;
+  margin-left: ${ASYNC_DATA_TABLE_FILTERS_WIDTH_PX}px;
+  overflow-y: scroll;
+  height: calc(100vh - ${TOPBAR_HEIGHT_PX}px - ${VIEW_HEADER_HEIGHT_PX}px);
 `
