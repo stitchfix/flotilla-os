dd4e19a5195eb504b3d0282e1bc31dd416413259
Patrick Sun <patricksun2011@gmail.com>
2018-11-10 18:18:14 -0800

    Add SecondaryText component

---
+++ ui/src/components/styled/SecondaryText.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import colors from "../../constants/colors"
+
+const SecondaryText = styled.div`
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  color: ${colors.gray[1]};
+  font-weight: 500;
+`
+
+export default SecondaryText
