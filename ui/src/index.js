afceac4baf45ef6d5e389d9ac39ebcfa8a2fc59e
Patrick Sun <patricksun2011@gmail.com>
2018-11-11 21:23:49 -0800

    bye redux

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,23 +1,12 @@
 import React, { Fragment } from "react"
 import { render } from "react-dom"
-import { Provider } from "react-redux"
-
-import Store from "./store"
 import App from "./components/App/App"
-import fetchSelectOpts from "./actions/fetchSelectOpts"
 import GlobalStyle from "./components/styled/GlobalStyle"
-
-const store = Store()
-
-// Dispatch global select options (group, tags, etc.)
-store.dispatch(fetchSelectOpts())
 
 render(
   <Fragment>
     <GlobalStyle />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </Fragment>,
   document.getElementById("root")
 )
--- ui/src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux"
-import selectOpts from "./selectOpts"
-
-export default combineReducers({ selectOpts })
