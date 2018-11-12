a645b5754705320d507a40e705cbd3c08feec066
Patrick Sun <patricksun2011@gmail.com>
2018-11-11 21:22:29 -0800

    Add group, cluster, and tag request methods to FlotillaAPIClient

--- ui/src/FlotillaAPIClient.js
+++ ui/src/FlotillaAPIClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
-import { isEmpty, isString, isObject } from "lodash"
+import { isEmpty, isString, isObject, isFunction, get } from "lodash"
 import qs from "qs"
 import urljoin from "url-join"
+import { stringToSelectOpt } from "./utils/reactSelectHelpers"
 
 /**
  * API client to communicate with the Flotilla API
@@ -9,15 +10,9 @@ import urljoin from "url-join"
 class FlotillaAPIClient {
   constructor({ location }) {
     this.location = location
-    this.getTasks = this.getTasks.bind(this)
-    this.getTask = this.getTask.bind(this)
-    this.getTaskHistory = this.getTaskHistory.bind(this)
-    this.getActiveRuns = this.getActiveRuns.bind(this)
-    this.createTask = this.createTask.bind(this)
-    this.updateTask = this.updateTask.bind(this)
   }
 
-  getTasks(query = { offset: 0, limit: 20 }) {
+  getTasks = (query = { offset: 0, limit: 20 }) => {
     return this._request({
       method: "get",
       path: "/v1/task",
@@ -26,7 +21,7 @@ class FlotillaAPIClient {
     })
   }
 
-  getTask({ definitionID }) {
+  getTask = ({ definitionID }) => {
     return this._request({
       method: "get",
       path: `/v1/task/${definitionID}`,
@@ -35,9 +30,7 @@ class FlotillaAPIClient {
     })
   }
 
-  getTaskHistory(
-    { definitionID, query } = { query: { limit: 20, offset: 0 } }
-  ) {
+  getTaskHistory = ({ definitionID, query }) => {
     return this._request({
       method: "get",
       path: `/v1/task/${definitionID}/history`,
@@ -46,7 +39,7 @@ class FlotillaAPIClient {
     })
   }
 
-  getActiveRuns(query = { offset: 0, limit: 20 }) {
+  getActiveRuns = (query = { offset: 0, limit: 20 }) => {
     return this._request({
       method: "get",
       path: "/v1/history",
@@ -55,7 +48,7 @@ class FlotillaAPIClient {
     })
   }
 
-  createTask({ values }) {
+  createTask = ({ values }) => {
     return this._request({
       method: "post",
       path: "/v1/task",
@@ -64,7 +57,7 @@ class FlotillaAPIClient {
     })
   }
 
-  updateTask({ definitionID, values }) {
+  updateTask = ({ definitionID, values }) => {
     return this._request({
       method: "put",
       path: `/v1/task/${definitionID}`,
@@ -73,7 +66,7 @@ class FlotillaAPIClient {
     })
   }
 
-  deleteTask({ definitionID }) {
+  deleteTask = ({ definitionID }) => {
     return this._request({
       method: "delete",
       path: `/v1/task/${definitionID}`,
@@ -82,7 +75,7 @@ class FlotillaAPIClient {
     })
   }
 
-  runTask({ definitionID, values }) {
+  runTask = ({ definitionID, values }) => {
     return this._request({
       method: "put",
       path: `/v4/task/${definitionID}/execute`,
@@ -91,7 +84,7 @@ class FlotillaAPIClient {
     })
   }
 
-  stopRun({ definitionID, runID }) {
+  stopRun = ({ definitionID, runID }) => {
     return this._request({
       method: "delete",
       path: `/v1/task/${definitionID}/history/${runID}`,
@@ -100,7 +93,7 @@ class FlotillaAPIClient {
     })
   }
 
-  getRun({ runID }) {
+  getRun = ({ runID }) => {
     return this._request({
       method: "get",
       path: `/v1/task/history/${runID}`,
@@ -109,7 +102,7 @@ class FlotillaAPIClient {
     })
   }
 
-  getRunLogs({ runID, lastSeen }) {
+  getRunLogs = ({ runID, lastSeen }) => {
     let q
 
     if (!!lastSeen) {
@@ -121,6 +114,36 @@ class FlotillaAPIClient {
       path: `/v1/${runID}/logs`,
       query: q,
       payload: null,
+    })
+  }
+
+  getGroups = () => {
+    return this._request({
+      method: "get",
+      path: `/v1/groups`,
+      query: { limit: 2000 },
+      payload: null,
+      preprocess: res => get(res, "groups", []).map(stringToSelectOpt),
+    })
+  }
+
+  getClusters = () => {
+    return this._request({
+      method: "get",
+      path: `/v1/clusters`,
+      query: null,
+      payload: null,
+      preprocess: res => get(res, "clusters", []).map(stringToSelectOpt),
+    })
+  }
+
+  getTags = () => {
+    return this._request({
+      method: "get",
+      path: `/v1/tags`,
+      query: { limit: 5000 },
+      payload: null,
+      preprocess: res => get(res, "tags", []).map(stringToSelectOpt),
     })
   }
 
@@ -138,7 +161,7 @@ class FlotillaAPIClient {
     return `${urljoin(this.location, path)}?${q}`
   }
 
-  _request({ method, path, query, payload }) {
+  _request({ method, path, query, payload, preprocess }) {
     return new Promise((resolve, reject) => {
       let config = { method, url: this._constructURL({ path, query }) }
 
@@ -148,6 +171,10 @@ class FlotillaAPIClient {
 
       axios(config)
         .then(res => {
+          if (isFunction(preprocess)) {
+            resolve(preprocess(res.data))
+          }
+
           resolve(res.data)
         })
         .catch(error => {
