efdd0f66709b06f22160d2f3b4dc8e89843b1a5d
Patrick Sun <patricksun2011@gmail.com>
2018-11-13 13:25:21 -0800

    RunView

--- ui/src/components/Run/RunView.js
+++ ui/src/components/Run/RunView.js
@@ -1,23 +1,45 @@
 import React from "react"
 import { get } from "lodash"
+import Navigation from "../Navigation/Navigation"
 import RunContext from "./RunContext"
 import RunSidebar from "./RunSidebar"
 import LogRequester from "./LogRequester"
 import View from "../styled/View"
+import intentTypes from "../../constants/intentTypes"
 
 const RunView = props => {
   return (
     <RunContext.Consumer>
       {ctx => {
+        const actions = [
+          {
+            isLink: true,
+            href: "/",
+            text: "Retry",
+          },
+          {
+            isLink: false,
+            text: "Stop Run",
+            intent: intentTypes.error,
+          },
+        ]
+        const breadcrumbs = [
+          {
+            text: get(ctx, ["data", "alias"], ""),
+            href: `/tasks/${get(ctx, ["data", "definition_id"])}`,
+          },
+          {
+            text: ctx.runID,
+            href: `/runs/${ctx.runID}`,
+          },
+        ]
         return (
           <View>
-            <div className="flot-detail-view flot-run-view">
-              {/* <RunSidebar /> */}
-              <LogRequester
-                runID={ctx.runID}
-                status={get(ctx, ["data", "status"])}
-              />
-            </div>
+            <Navigation actions={actions} breadcrumbs={breadcrumbs} />
+            <LogRequester
+              runID={ctx.runID}
+              status={get(ctx, ["data", "status"])}
+            />
           </View>
         )
       }}
