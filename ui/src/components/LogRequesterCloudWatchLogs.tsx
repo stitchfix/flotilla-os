ccaaabb9121d80683680680ed2933efc8456aae0
Patrick Sun <patricksun2011@gmail.com>
2019-12-23 12:00:39 -0800

    cleanup LogRequesterCloudWatchLogs

--- ui/src/components/LogRequesterCloudWatchLogs.tsx
+++ ui/src/components/LogRequesterCloudWatchLogs.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { has, isEmpty } from "lodash"
 import api from "../api"
 import LogRenderer from "./LogRenderer"
-import { LogChunk, RunStatus, RunLog } from "../types"
+import { RunStatus, RunLog } from "../types"
 import { LOG_FETCH_INTERVAL_MS } from "../constants"
 import ErrorCallout from "./ErrorCallout"
 
@@ -15,7 +15,7 @@ type Props = {
 }
 
 type State = {
-  logs: LogChunk[]
+  logs: string
   lastSeen: string | undefined
   isLoading: boolean
   error: any
@@ -23,14 +23,14 @@ type State = {
 }
 
 const initialState: State = {
-  logs: [],
+  logs: "",
   lastSeen: undefined,
   isLoading: false,
   error: false,
   hasLogs: false,
 }
 
-class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
+class LogRequesterCloudWatchLogs extends React.Component<Props, State> {
   private requestInterval: number | undefined
   state = initialState
 
@@ -44,6 +44,7 @@ class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
       return
     }
 
+    // Stop request interval if run transitions from running to stopped.
     if (
       prevProps.status !== RunStatus.STOPPED &&
       this.props.status === RunStatus.STOPPED
@@ -71,6 +72,10 @@ class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
     window.clearInterval(this.requestInterval)
   }
 
+  /**
+   * Performs one initial API call to the logs endpoint and starts a request
+   * interval if the run is not stopped.
+   */
   initialize() {
     this.requestLogs()
 
@@ -79,6 +84,10 @@ class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
     }
   }
 
+  /**
+   * Clears the request interval, resets the component state, and calls
+   * this.initialize.
+   */
   handleRunIDChange() {
     // Clear request interval
     this.clearRequestInterval()
@@ -98,114 +107,53 @@ class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
 
     api
       .getRunLog({ runID, lastSeen })
-      .then(this.handleResponse)
+      .then((res: RunLog) => {
+        this.handleResponse(res)
+      })
       .catch(error => {
         this.clearRequestInterval()
         this.setState({ error })
       })
   }
 
-  /**
-   * The response handler for the logs endpoint performs the following:
-   * 1. Calls this.shouldAppendLogsToState to determine if a valid log chunk
-   * was received.
-   * 2. Once the logs have been appended, it checks to see if it should
-   * "exhaust" the logs endpoint - meaning that it will continuously hit the
-   * logs endpoint until all remaining logs have been fetched.
-   */
-  handleResponse = async (response: RunLog) => {
-    this.setState({ isLoading: false, error: false })
+  handleResponse = (res: RunLog) => {
+    const PREV_LAST_SEEN = this.state.lastSeen
+    this.setState(
+      prev => {
+        const isLoading = false
+        const error = false
 
-    // Return if there are no logs.
-    if (!has(response, "log") || isEmpty(response.log)) {
-      return
-    }
+        // Return if there are no logs.
+        if (!has(res, "log") || isEmpty(res.log)) {
+          return { ...prev, isLoading, error }
+        }
 
-    const shouldAppendLogsToState = await this.shouldAppendLogsToState(response)
+        let logs = prev.logs
+        let lastSeen: string | undefined = res.last_seen
+        let hasLogs = prev.hasLogs || res.log.length > 0
 
-    if (shouldAppendLogsToState === true) {
-      this.appendLogsToState(response).then(prevLastSeen => {
+        // Append logs if necessary.
+        if (res.last_seen && res.last_seen !== prev.lastSeen) {
+          logs += res.log
+        }
+
+        return { ...prev, isLoading, error, logs, lastSeen, hasLogs }
+      },
+      () => {
         if (
-          this.hasRunFinished() &&
-          this.hasAdditionalLogs({ prevLastSeen, response })
+          this.props.status === RunStatus.STOPPED &&
+          (!PREV_LAST_SEEN || res.last_seen !== PREV_LAST_SEEN)
         ) {
-          this.requestLogs()
-        }
-      })
-    }
-  }
-
-  /**
-   * Returns a Promise that resolves with whether logs should be appended to
-   * the component's state.
-   *
-   * Note: this probably should just return a boolean but I haven't had time
-   * to test what that would look like on long running jobs.
-   */
-  shouldAppendLogsToState = (response: RunLog): Promise<boolean> =>
-    new Promise(resolve => {
-      const { lastSeen } = this.state
-
-      if (response.last_seen === lastSeen) {
-        resolve(false)
-      }
-
-      resolve(true)
-    })
-
-  /**
-   * Returns a Promise that appends a new LogChunk object to the component's
-   * state and resolves with the previous state's `lastSeen` attribute. The
-   * previous state's lastSeen is then used to determine whether or not there
-   * are additional logs still waiting to be fetched.
-   */
-  appendLogsToState = (response: RunLog): Promise<string | undefined> =>
-    new Promise((resolve, reject) => {
-      // Create a new LogChunk object.
-      const chunk: LogChunk = {
-        chunk: response.log,
-        lastSeen: response.last_seen,
-      }
-
-      let prevLastSeen: string | undefined
-
-      // Append it to state.logs
-      this.setState(
-        prevState => {
-          const prevHasLogs = prevState.hasLogs
-          prevLastSeen = prevState.lastSeen
-          return {
-            logs: [...prevState.logs, chunk],
-            lastSeen: response.last_seen,
-            hasLogs: prevHasLogs === true ? true : response.log.length > 0,
+          if (has(res, "last_seen")) {
+            this.requestLogs()
           }
-        },
-        () => {
-          resolve(prevLastSeen)
         }
-      )
-    })
-
-  hasAdditionalLogs = ({
-    prevLastSeen,
-    response,
-  }: {
-    prevLastSeen: string | undefined
-    response: RunLog
-  }): boolean => {
-    if (!prevLastSeen || response.last_seen !== prevLastSeen) {
-      if (has(response, "last_seen")) {
-        return true
       }
-    }
-
-    return false
+    )
   }
-
-  hasRunFinished = (): boolean => this.props.status === RunStatus.STOPPED
 
   render() {
-    const { height } = this.props
+    const { height, status, shouldAutoscroll } = this.props
     const { isLoading, error, logs } = this.state
 
     if (error) return <ErrorCallout error={error} />
@@ -213,10 +161,10 @@ class LogRequesterCloudWatchLogs extends React.PureComponent<Props, State> {
     return (
       <LogRenderer
         height={height}
-        logs={logs.map(({ chunk }) => chunk)}
-        hasRunFinished={this.hasRunFinished()}
+        logs={logs}
+        hasRunFinished={status === RunStatus.STOPPED}
         isLoading={isLoading}
-        shouldAutoscroll={this.props.shouldAutoscroll}
+        shouldAutoscroll={shouldAutoscroll}
       />
     )
   }
