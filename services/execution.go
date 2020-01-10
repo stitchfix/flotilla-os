a48c707c1d702b0b91fd5f161340de88e0f0c2c7
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2020-01-10 22:01:34 +0000

    fixing deletion of jobs in queued state

--- services/execution.go
+++ services/execution.go
@@ -428,7 +428,7 @@ func (es *executionService) Terminate(runID string, userInfo state.UserInfo) err
 	}
 
 	// If it's queued and not submitted, set status to stopped (checked by submit worker)
-	if run.Status == state.StatusQueued {
+	if run.Status == state.StatusQueued && *run.Engine == state.ECSEngine {
 		_, err = es.stateManager.UpdateRun(runID, state.Run{Status: state.StatusStopped})
 		return err
 	}
