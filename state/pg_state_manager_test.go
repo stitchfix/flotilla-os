d98c55f0ca6239caf80345ebffbd40ed87a01736
Patrick Sun <patricksun2011@gmail.com>
2020-01-22 17:39:13 -0800

    drop definition_template during test teardown process

--- state/pg_state_manager_test.go
+++ state/pg_state_manager_test.go
@@ -116,7 +116,7 @@ func tearDown() {
 	db := getDB(conf)
 	db.MustExec(`
     drop table if exists
-      task, task_def, task_def_ports, task_status, task_def_tags, tags
+      task, task_def, task_def_ports, task_status, task_def_tags, tags, definition_template
     cascade;
     drop sequence if exists task_status_status_id_seq;
     `)
