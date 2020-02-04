2d7dbac8cf056c1a3f766a799d9d6bb0326dbc6f
Patrick Sun <patricksun2011@gmail.com>
2020-02-03 18:22:26 -0800

    Revert "wip"
    This reverts commit b57f1bf77c3238cf612dea2bb7b17de27c0e8a0a.

--- .migrations/V20200123092100__definition_templates.sql
+++ .migrations/V20200123092100__definition_templates.sql
@@ -1,18 +1,22 @@
-CREATE TABLE IF NOT EXISTS template_definition (
+--
+-- Task Types
+--
+CREATE TABLE IF NOT EXISTS definition_template (
   id VARCHAR PRIMARY KEY,
   type VARCHAR NOT NULL,
   version INTEGER NOT NULL,
   schema JSONB NOT NULL,
-  command_template TEXT NOT NULL,
-  definition_id REFERENCES task_def(definition_id),
+  template TEXT NOT NULL,
+  image VARCHAR NOT NULL,
   CONSTRAINT definition_template_type_version UNIQUE(type, version)
 );
 
-CREATE TABLE IF NOT EXISTS template_run (
-  template_definition_id VARCHAR REFERENCES template_definition(id),
-  run_id VARCHAR REFERENCES task(run_id),
-  template_arguments JSONB
-);
+ALTER TABLE task_def
+  DROP COLUMN task_type,
+  ADD COLUMN template_id character varying REFERENCES definition_template(id),
+  ADD COLUMN template_payload jsonb;
 
-ALTER TABLE task_def DROP COLUMN task_type;
-ALTER TABLE task DROP COLUMN task_type;
+ALTER TABLE task
+  DROP COLUMN task_type,
+  ADD COLUMN template_id character varying REFERENCES definition_template(id),
+  ADD COLUMN template_payload jsonb;
