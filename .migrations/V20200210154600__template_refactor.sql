625d0fb2524c7ebf2a788587eb4335aabb9fa097
Patrick Sun <patricksun2011@gmail.com>
2020-02-10 16:00:33 -0800

    oh geez

--- .migrations/V20200210154600__template_refactor.sql
+++ .migrations/V20200210154600__template_refactor.sql
@@ -1,4 +1,4 @@
 ALTER TABLE template
   RENAME COLUMN type to template_name
-  DELETE CONSTRAINT template_type_version
+  DROP CONSTRAINT template_type_version
   ADD CONSTRAINT template_name_version UNIQUE(template_name, version);
