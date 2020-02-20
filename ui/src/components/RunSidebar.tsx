ad64d3533048813cad8ce41b2040da3c81c8bc1a
Patrick Sun <patricksun2011@gmail.com>
2020-02-20 14:35:06 -0800

    json view

--- ui/src/components/RunSidebar.tsx
+++ ui/src/components/RunSidebar.tsx
@@ -1,32 +1,54 @@
 import * as React from "react"
 import { get } from "lodash"
-import { Card } from "@blueprintjs/core"
+import { Card, Button, Colors } from "@blueprintjs/core"
+import JsonView from "react-json-view"
 import { ExecutionEngine, Run, ExecutableType } from "../types"
 import EnvList from "./EnvList"
 import RunAttributes from "./RunAttributes"
 import RunDebugAttributes from "./RunDebugAttributes"
-import RecursiveKeyValueRenderer from "./RecursiveKeyValueRenderer"
 
-const RunSidebar: React.FC<{ data: Run }> = ({ data }) => (
-  <div className="flotilla-sidebar-view-sidebar">
-    {data && data.executable_type === ExecutableType.ExecutableTypeTemplate && (
-      <Card style={{ marginBottom: 12 }}>
-        <RecursiveKeyValueRenderer
-          data={get(data, ["execution_request_custom", "template_payload"], {})}
-        />
+const RunSidebar: React.FC<{ data: Run }> = ({ data }) => {
+  const templatePayload = get(
+    data,
+    ["execution_request_custom", "template_payload"],
+    {}
+  )
+
+  return (
+    <div className="flotilla-sidebar-view-sidebar">
+      {data && data.executable_type === ExecutableType.ExecutableTypeTemplate && (
+        <Card style={{ marginBottom: 12 }}>
+          <div className="flotilla-card-header-container">
+            <div className="flotilla-card-header">Template Payload</div>
+          </div>
+          <JsonView
+            src={templatePayload}
+            name={false}
+            collapsed={2}
+            enableClipboard={false}
+            displayDataTypes={false}
+            displayObjectSize={false}
+            theme="ocean"
+            style={{
+              background: Colors.DARK_GRAY1,
+              fontFamily: "Roboto Mono",
+              fontSize: "0.8rem",
+            }}
+          />
+        </Card>
+      )}
+      <RunAttributes data={data} />
+      <Card>
+        <div className="flotilla-card-header-container">
+          <div className="flotilla-card-header">Environment Variables</div>
+        </div>
+        <EnvList env={data.env} />
       </Card>
-    )}
-    <RunAttributes data={data} />
-    <Card>
-      <div className="flotilla-card-header-container">
-        <div className="flotilla-card-header">Environment Variables</div>
-      </div>
-      <EnvList env={data.env} />
-    </Card>
-    {data && data.engine === ExecutionEngine.EKS && (
-      <RunDebugAttributes data={data} />
-    )}
-  </div>
-)
+      {data && data.engine === ExecutionEngine.EKS && (
+        <RunDebugAttributes data={data} />
+      )}
+    </div>
+  )
+}
 
 export default RunSidebar
