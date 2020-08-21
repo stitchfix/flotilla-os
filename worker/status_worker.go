6197ce16824bf0041c2bafe4ef9488419b27e34e
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2020-08-22 01:41:45 +0200

    remove rand int

--- worker/status_worker.go
+++ worker/status_worker.go
@@ -105,7 +105,7 @@ func (sw *statusWorker) runOnceEKS() {
 func (sw *statusWorker) processEKSRuns(runs []state.Run) {
 	var lockedRuns []state.Run
 	for _, run := range runs {
-		duration := time.Duration(rand.Intn(60)) * time.Second
+		duration := time.Duration(30) * time.Second
 		lock := sw.acquireLock(run, "status", duration)
 		if lock {
 			lockedRuns = append(lockedRuns, run)
