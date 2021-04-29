4cb7c05b9354e6d24008064cfca40dd94683dd7b
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2021-04-29 18:06:51 +0000

    handle exit code for timeout and also add case for job completed successfully

--- worker/events_worker.go
+++ worker/events_worker.go
@@ -112,8 +112,17 @@ func (ew *eventsWorker) processEvent(kubernetesEvent state.KubernetesEvent) {
 
 		if kubernetesEvent.Reason == "DeadlineExceeded" {
 			run.ExitReason = &kubernetesEvent.Message
+			exitCode := int64(124)
+			run.ExitCode = &exitCode
+			run.Status = state.StatusStopped
 		}
 
+		if kubernetesEvent.Reason == "Completed" {
+			run.ExitReason = &kubernetesEvent.Message
+			exitCode := int64(0)
+			run.ExitCode = &exitCode
+			run.Status = state.StatusStopped
+		}
 		run, err = ew.sm.UpdateRun(runId, run)
 		if err != nil {
 			_ = ew.log.Log("message", "error saving kubernetes events", "run", runId, "error", fmt.Sprintf("%+v", err))
