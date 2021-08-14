358b887b6dde8cab20f0ea71be0e82b940a265c9
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2021-08-14 05:35:28 +0000

    wip

--- worker/events_worker.go
+++ worker/events_worker.go
@@ -93,6 +93,7 @@ func (ew *eventsWorker) runOnce() {
 	ew.processEvent(kubernetesEvent)
 }
 func (ew *eventsWorker) processEventEMR(kubernetesEvent state.KubernetesEvent) {
+	defer kubernetesEvent.Done()
 	if kubernetesEvent.InvolvedObject.Kind == "Pod" {
 		pod, err := ew.kClient.CoreV1().Pods(kubernetesEvent.InvolvedObject.Namespace).Get(kubernetesEvent.InvolvedObject.Name, metav1.GetOptions{})
 		var emrJobId *string = nil
@@ -145,7 +146,6 @@ func (ew *eventsWorker) processEventEMR(kubernetesEvent state.KubernetesEvent) {
 			}
 		}
 	}
-	_ = kubernetesEvent.Done()
 }
 func (ew *eventsWorker) processEvent(kubernetesEvent state.KubernetesEvent) {
 	runId := kubernetesEvent.InvolvedObject.Labels.JobName
