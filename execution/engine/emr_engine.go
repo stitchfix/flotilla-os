26625eca1e1e9a7c77adfb382a46c6605fbb2961
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2021-10-02 17:05:07 +0000

    Remove verbose shell logging from Flotilla's cmd slice

--- execution/engine/emr_engine.go
+++ execution/engine/emr_engine.go
@@ -553,6 +553,6 @@ func (emr *EMRExecutionEngine) constructCmdSlice(command *string) []string {
 	}
 	bashCmd := "bash"
 	optLogin := "-l"
-	optStr := "-cex"
+	optStr := "-ce"
 	return []string{bashCmd, optLogin, optStr, cmdString}
 }
