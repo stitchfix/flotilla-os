3386dfe262280759e25310c1aa2958447efd3d16
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2021-10-17 04:19:41 +0000

    keep pod events after the job finishes

--- worker/events_worker.go
+++ worker/events_worker.go
@@ -131,9 +131,9 @@ func (ew *eventsWorker) processEventEMR(emrEvent state.EmrEvent) {
 				run.StartedAt = run.QueuedAt
 			}
 			run.ExitReason = emrEvent.Detail.StateDetails
-			var events state.PodEvents
+			// var events state.PodEvents
 			// Pod Events are verbose and should be only stored for failed or running jobs.
-			run.PodEvents = &events
+			// run.PodEvents = &events
 		case "RUNNING":
 			run.Status = state.StatusRunning
 			run.StartedAt = &timestamp
