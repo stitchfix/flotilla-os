3de994b9d6c0e185cb45fe13ed5ddb084d118661
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2022-03-07 11:23:59 -0800

    Refactor map/list configs

--- services/execution_test.go
+++ services/execution_test.go
@@ -187,11 +187,7 @@ func TestExecutionService_CreateDefinitionRunByAlias(t *testing.T) {
 	if len(run.RunID) == 0 {
 		t.Errorf("Expected Create to populated run with non-empty RunID")
 	}
-
-	if run.ClusterName != "clusta" {
-		t.Errorf("Expected cluster name 'clusta' but was '%s'", run.ClusterName)
-	}
-
+	
 	if run.DefinitionID != "B" {
 		t.Errorf("Expected definitionID 'B' but was '%s'", run.DefinitionID)
 	}
