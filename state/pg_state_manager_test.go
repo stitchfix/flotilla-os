df6cea33278f4a8e74e7afeef157a8fbc515cfd2
Ujjwal Sarin <ujjwal.sarin@stitchfix.com>
2022-10-10 19:52:40 +0000

    wip

--- state/pg_state_manager_test.go
+++ state/pg_state_manager_test.go
@@ -54,9 +54,9 @@ func insertDefinitions(db *sqlx.DB) {
 	taskSQL := `
     INSERT INTO task (
       run_id, definition_id, cluster_name, alias, image, exit_code, status,
-      started_at, finished_at, instance_id, instance_dns_name, group_name, env, engine
+      started_at, finished_at, instance_id, instance_dns_name, group_name, env, engine, "user"
     ) VALUES (
-      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, 'eks'
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, 'eks', 'foo'
     )
     `
 
