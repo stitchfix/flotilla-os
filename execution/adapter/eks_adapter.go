1c657b029f1c156d8bb6a7f45cef2413f8d79df5
Oz Raza <uraza@stitchfix.com>
2023-01-24 14:11:53 -0800

    lowering resource thresholds

--- execution/adapter/eks_adapter.go
+++ execution/adapter/eks_adapter.go
@@ -217,7 +217,7 @@ func (a *eksAdapter) constructAffinity(executable state.Executable, run state.Ru
 			Values:   gpuNodeTypes,
 		})
 		//adding node group affinities for non-gpu runs
-		if *run.Memory < 32000 && *run.Cpu < 8000 {
+		if *run.Memory < 12750 && *run.Cpu < 3800 {
 			requiredMatch = append(requiredMatch, corev1.NodeSelectorRequirement{
 				Key:      "sfix/instance.size",
 				Operator: corev1.NodeSelectorOpIn,
