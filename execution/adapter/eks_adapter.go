3bd45de86542f97b6d63cc3254a9ac45cbdc8d9c
Oz Raza <uraza@stitchfix.com>
2023-01-27 14:45:35 -0800

    tweaking min memory val for small

--- execution/adapter/eks_adapter.go
+++ execution/adapter/eks_adapter.go
@@ -217,7 +217,7 @@ func (a *eksAdapter) constructAffinity(executable state.Executable, run state.Ru
 			Values:   gpuNodeTypes,
 		})
 		//adding node group affinities for non-gpu runs
-		if *run.Memory < 12750 && *run.Cpu < 3800 {
+		if *run.Memory < 12000 && *run.Cpu < 3800 {
 			requiredMatch = append(requiredMatch, corev1.NodeSelectorRequirement{
 				Key:      "sfix/instance.size",
 				Operator: corev1.NodeSelectorOpIn,
