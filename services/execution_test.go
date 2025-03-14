6aa731ce156f86bd8ef6f75a940dd5d1632c4a13
william.hume@stitchfix.com <william.hume@stitchfix.com>
2025-03-14 15:37:06 -0400

    add a new call to the test

--- services/execution_test.go
+++ services/execution_test.go
@@ -56,6 +56,7 @@ func TestExecutionService_CreateDefinitionRunByDefinitionID(t *testing.T) {
 		"GetPodReAttemptRate":      true,
 		"Enqueue":                  true,
 		"ListClusterStates":        true,
+		"clusterSupportsTier":      true,
 	}
 
 	cmd := "_test_cmd_"
