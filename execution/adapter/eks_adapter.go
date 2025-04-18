5b42e2ff3182bac075205cd96eb474a1aa0573c4
Evan Zacks <evan.zacks@stitchfix.com>
2025-04-18 10:15:41 -0700

    Set ImagePullPolicy on container to better support mutable images

--- execution/adapter/eks_adapter.go
+++ execution/adapter/eks_adapter.go
@@ -107,12 +107,13 @@ func (a *eksAdapter) AdaptFlotillaDefinitionAndRunToJob(executable state.Executa
 	volumeMounts, volumes := a.constructVolumeMounts(executable, run, manager, araEnabled)
 
 	container := corev1.Container{
-		Name:      run.RunID,
-		Image:     run.Image,
-		Command:   cmdSlice,
-		Resources: resourceRequirements,
-		Env:       a.envOverrides(executable, run),
-		Ports:     a.constructContainerPorts(executable),
+		Name:            run.RunID,
+		Image:           run.Image,
+		Command:         cmdSlice,
+		Resources:       resourceRequirements,
+		Env:             a.envOverrides(executable, run),
+		Ports:           a.constructContainerPorts(executable),
+		ImagePullPolicy: corev1.PullAlways,
 	}
 
 	if volumeMounts != nil {
